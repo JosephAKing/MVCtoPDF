4691061d4901c3160cc1b0e89dc556b5f4212ccb
jking <jking@DESKTOP-ALPO3HU>
2016-05-03 16:46:04 -0400

    Add project files.

---
+++ MVCtoPDF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCtoPDF.Startup))]
+namespace MVCtoPDF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
